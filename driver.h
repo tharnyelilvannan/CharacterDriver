56578e45fde99b69ed19533e223140bcf706ba31
tharnyelilvannan <tharny2005@outlook.com>
2025-12-25 13:59:00 -0500

    Add header file

---
+++ driver.h
@@ -0,0 +1,9 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
